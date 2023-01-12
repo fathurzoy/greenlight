6cd5d610f6a62ac3483de9fdfa9b11db153163af
Fathur Rahman <fathur.rahman@udana.id>
2023-01-12 15:23:28 +0700

    Working with SQL Migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,6 +13,7 @@ import (
 	// Import the pq driver so that it can register itself with the database/sql
 	// package. Note that we alias this import to the blank identifier, to stop the Go
 	// compiler complaining that the package isn't being used.
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/lib/pq"
 )
 const version = "1.0.0"
