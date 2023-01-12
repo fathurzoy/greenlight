0567f248c6eb2db8d9724e473dc596e636822dd0
Fathur Rahman <fathur.rahman@udana.id>
2023-01-12 15:32:56 +0700

    CRUD Operations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,8 +13,9 @@ import (
 	// Import the pq driver so that it can register itself with the database/sql
 	// package. Note that we alias this import to the blank identifier, to stop the Go
 	// compiler complaining that the package isn't being used.
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	// _ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/lib/pq"
+	"greenlight.alexedwards.net/internal/data"
 )
 const version = "1.0.0"
 // Add maxOpenConns, maxIdleConns and maxIdleTime fields to hold the configuration
@@ -29,55 +30,46 @@ type config struct {
 			maxIdleTime  string
 	}
 }
-
+// Add a models field to hold our new Models struct.
 type application struct {
-    config config
-    logger *log.Logger
+	config config
+	logger *log.Logger
+	models data.Models
 }
+
 func main() {
-    var cfg config
-		flag.IntVar(&cfg.port, "port", 4000, "API server port")
-		flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-    // Read the DSN value from the db-dsn command-line flag into the config struct. We
-    // default to using our development DSN if no flag is provided.
-    flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
-    // flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
-     // Read the connection pool settings from command-line flags into the config struct.
-		// Notice the default values that we're using?
-		flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
-		flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
-		flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
-		flag.Parse()
-    logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
-    // Call the openDB() helper function (see below) to create the connection pool,
-    // passing in the config struct. If this returns an error, we log it and exit the
-    // application immediately.
-		db, err := openDB(cfg)
-    if err != nil {
-        logger.Fatal(err)
-    }
-    // Defer a call to db.Close() so that the connection pool is closed before the
-    // main() function exits.
-    defer db.Close()
-    // Also log a message to say that the connection pool has been successfully
-    // established.
-    logger.Printf("database connection pool established")
-    app := &application{
-        config: cfg,
-        logger: logger,
-    }
-    srv := &http.Server{
-        Addr:         fmt.Sprintf(":%d", cfg.port),
-        Handler:      app.routes(),
-        IdleTimeout:  time.Minute,
-        ReadTimeout:  10 * time.Second,
-        WriteTimeout: 30 * time.Second,
-    }
-    logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-    // Because the err variable is now already declared in the code above, we need
-    // to use the = operator here, instead of the := operator.
-    err = srv.ListenAndServe()
-    logger.Fatal(err)
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.Parse()
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	db, err := openDB(cfg)
+	if err != nil {
+			logger.Fatal(err)
+	}
+	defer db.Close()
+	logger.Printf("database connection pool established")
+	// Use the data.NewModels() function to initialize a Models struct, passing in the
+	// connection pool as a parameter.
+	app := &application{
+			config: cfg,
+			logger: logger,
+			models: data.NewModels(db),
+	}
+	srv := &http.Server{
+			Addr:         fmt.Sprintf(":%d", cfg.port),
+			Handler:      app.routes(),
+			IdleTimeout:  time.Minute,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 30 * time.Second,
+	}
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err = srv.ListenAndServe()
+	logger.Fatal(err)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"greenlight.alexedwards.net/internal/validator" // New import
@@ -27,3 +28,40 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
     v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
     v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
+
+// Define a MovieModel struct type which wraps a sql.DB connection pool.
+type MovieModel struct {
+    DB *sql.DB
+}
+// Add a placeholder method for inserting a new record in the movies table.
+func (m MovieModel) Insert(movie *Movie) error {
+    return nil
+}
+// Add a placeholder method for fetching a specific record from the movies table.
+func (m MovieModel) Get(id int64) (*Movie, error) {
+    return nil, nil
+}
+// Add a placeholder method for updating a specific record in the movies table.
+func (m MovieModel) Update(movie *Movie) error {
+    return nil
+}
+// Add a placeholder method for deleting a specific record from the movies table.
+func (m MovieModel) Delete(id int64) error {
+    return nil
+}
+
+
+
+// type MockMovieModel struct{}
+// func (m MockMovieModel) Insert(movie *Movie) error {
+//     // Mock the action...
+// }
+// func (m MockMovieModel) Get(id int64) (*Movie, error) {
+//     // Mock the action...
+// }
+// func (m MockMovieModel) Update(movie *Movie) error {
+//     // Mock the action...
+// }
+// func (m MockMovieModel) Delete(id int64) error {
+//     // Mock the action...
+// }
