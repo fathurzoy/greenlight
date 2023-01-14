d5eb1b4cf8e53bb1ccc729f66ac33d2ad4bdf172
Fathur Rahman <fathur.rahman@udana.id>
2023-01-14 19:23:14 +0700

    find by id api handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"greenlight.alexedwards.net/internal/data"
 	"greenlight.alexedwards.net/internal/validator" // New import
@@ -57,21 +57,24 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-			// Use the new notFoundResponse() helper.
 			app.notFoundResponse(w, r)
 			return
 	}
-	movie := data.Movie{
-			ID:        id,
-			CreatedAt: time.Now(),
-			Title:     "Casablanca",
-			Runtime:   102,
-			Genres:    []string{"drama", "romance", "war"},
-			Version:   1,
+	// Call the Get() method to fetch the data for a specific movie. We also need to
+	// use the errors.Is() function to check if it returns a data.ErrRecordNotFound
+	// error, in which case we send a 404 Not Found response to the client.
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+					app.notFoundResponse(w, r)
+			default:
+					app.serverErrorResponse(w, r, err)
+			}
+			return
 	}
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-			// Use the new serverErrorResponse() helper.
 			app.serverErrorResponse(w, r, err)
 	}
 }
