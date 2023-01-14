d3fc1756076a6e90c19faddf035ebb8c59db9a57
Fathur Rahman <fathur.rahman@udana.id>
2023-01-14 23:02:57 +0700

    Panic Recovery

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,17 +6,17 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-
-func (app *application) routes() *httprouter.Router {
+// Update the routes() method to return a http.Handler instead of a *httprouter.Router.
+func (app *application) routes() http.Handler {
     router := httprouter.New()
     router.NotFound = http.HandlerFunc(app.notFoundResponse)
     router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
     router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-    // Add the route for the GET /v1/movies endpoint.
     router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
     router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
     router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
     router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
     router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
-    return router
+    // Wrap the router with the panic recovery middleware.
+    return app.recoverPanic(router)
 }
