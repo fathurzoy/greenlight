1063f4221ebbfb3e2ef7743fdc2102f39b887305
Fathur Rahman <fathur.rahman@udana.id>
2023-01-14 22:00:17 +0700

    Sorting Lists Implementing sorting

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,16 +1,36 @@
 package data
 
 import (
-	"greenlight.alexedwards.net/internal/validator" // New import
-)
+	"strings" // New import
 
-// Add a SortSafelist field to hold the supported sort values.
+	"greenlight.alexedwards.net/internal/validator"
+)
 type Filters struct {
     Page         int
     PageSize     int
     Sort         string
     SortSafelist []string
 }
+// Check that the client-provided Sort field matches one of the entries in our safelist
+// and if it does, extract the column name from the Sort field by stripping the leading
+// hyphen character (if one exists).
+func (f Filters) sortColumn() string {
+    for _, safeValue := range f.SortSafelist {
+        if f.Sort == safeValue {
+            return strings.TrimPrefix(f.Sort, "-")
+        }
+    }
+    panic("unsafe sort parameter: " + f.Sort)
+}
+// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the
+// Sort field.
+func (f Filters) sortDirection() string {
+    if strings.HasPrefix(f.Sort, "-") {
+        return "DESC"
+    }
+    return "ASC"
+}
+
 func ValidateFilters(v *validator.Validator, f Filters) {
     // Check that the page and page_size parameters contain sensible values.
     v.Check(f.Page > 0, "page", "must be greater than zero")
