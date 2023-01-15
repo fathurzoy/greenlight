b7fb7cfc1a6af8ae8fcad70cf06d82bfb737259c
Fathur Rahman <fathur.rahman@udana.id>
2023-01-15 07:39:33 +0700

    Sending Emails

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -14,6 +14,7 @@ import (
 	_ "github.com/lib/pq"
 	"greenlight.alexedwards.net/internal/data"
 	"greenlight.alexedwards.net/internal/jsonlog"
+	"greenlight.alexedwards.net/internal/mailer"
 )
 const version = "1.0.0"
 // Add maxOpenConns, maxIdleConns and maxIdleTime fields to hold the configuration
@@ -27,24 +28,26 @@ type config struct {
 			maxIdleConns int
 			maxIdleTime  string
 	}
-	// Add a new limiter struct containing fields for the requests-per-second and burst
-	// values, and a boolean field which we can use to enable/disable rate limiting
-	// altogether.
 	limiter struct {
+			enabled bool
 			rps     float64
 			burst   int
-			enabled bool
+	}
+	smtp struct {
+			host     string
+			port     int
+			username string
+			password string
+			sender   string
 	}
 }
-// Add a models field to hold our new Models struct.
-// Change the logger field to have the type *jsonlog.Logger, instead of
-// *log.Logger.
+// Update the application struct to hold a new Mailer instance.
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
-
 func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
@@ -53,41 +56,38 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
-
-	// go run ./cmd/api/ -limiter-burst=2
-	// go run ./cmd/api/ -limiter-enabled=false
-	// Create command line flags to read the setting values into the config struct.
-  // Notice that we use true as the default for the 'enabled' setting?
-  flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
-  flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
-  flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	// Read the SMTP server configuration settings into the config struct, using the
+	// Mailtrap settings as the default values. IMPORTANT: If you're following along,
+	// make sure to replace the default values for smtp-username and smtp-password
+	// with your own Mailtrap credentials.
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "eb6adbcac0cbab", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "75c9348a74ca80", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
 	flag.Parse()
-	// Initialize a new jsonlog.Logger which writes any messages *at or above* the INFO
-	// severity level to the standard out stream.
-	// Initialize the custom logger.
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
-		
 	db, err := openDB(cfg)
 	if err != nil {
-			// Use the PrintFatal() method to write a log entry containing the error at the
-			// FATAL level and exit. We have no additional properties to include in the log
-			// entry, so we pass nil as the second parameter.
 			logger.PrintFatal(err, nil)
 	}
 	defer db.Close()
-	// Likewise use the PrintInfo() method to write a message at the INFO level.
 	logger.PrintInfo("database connection pool established", nil)
+	// Initialize a new Mailer instance using the settings from the command line
+	// flags, and add it to the application struct.
 	app := &application{
-		config: cfg,
-		logger: logger,
-		models: data.NewModels(db),
+			config: cfg,
+			logger: logger,
+			models: data.NewModels(db),
+			mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
-	// Call app.serve() to start the server.
 	err = app.serve()
 	if err != nil {
 			logger.PrintFatal(err, nil)
 	}
-
 }
 
 func openDB(cfg config) (*sql.DB, error) {
