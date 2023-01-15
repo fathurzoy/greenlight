bf6c44e3a25eb7dfdc5eabfb2053529f5a2548b1
Fathur Rahman <fathur.rahman@udana.id>
2023-01-15 07:43:59 +0700

    Retrying email send attempts

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -77,4 +77,23 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
       app.serverErrorResponse(w, r, err)
   }
 
+	// Launch a goroutine which runs an anonymous function that sends the welcome email.
+	go func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+				// Importantly, if there is an error sending the email then we use the
+				// app.logger.PrintError() helper to manage it, instead of the
+				// app.serverErrorResponse() helper like before.
+				app.logger.PrintError(err, nil)
+		}
+	}()
+	// Note that we also change this to send the client a 202 Accepted status code.
+	// This status code indicates that the request has been accepted for processing, but
+	// the processing has not been completed.
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
+	if err != nil {
+			app.serverErrorResponse(w, r, err)
+	}
+
+
 }
